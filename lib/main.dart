9b46320934e8d7c4705bc1e84f67c9d8be20992b
NakhimChea <cheanakhimzzzz@gmail.com>
2022-01-17 17:04:31 +0700

    Initialized Firebase Core

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:travenx_loitafoundation/config/variable.dart';
 import 'package:travenx_loitafoundation/providers/responsive_widget.dart';
 import 'package:travenx_loitafoundation/providers/theme_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      // options: FirebaseOptions(
+      //   apiKey: "AIzaSyAdhhJKdC5-eH66MjJelC-VEeEaez4Xu0M",
+      //   authDomain: "travenx.firebaseapp.com",
+      //   projectId: "travenx",
+      //   storageBucket: "travenx.appspot.com",
+      //   messagingSenderId: "757818286951",
+      //   appId: "1:757818286951:web:264d197c1572e7f1b905c2",
+      //   measurementId: "G-96JT3Z7SBV",
+      // ),
+      );
   runApp(MyApp());
 }
 

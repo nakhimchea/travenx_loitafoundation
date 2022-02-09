4d2ad741d5814204e462c8df73078f94e343c431
NakhimChea <cheanakhimzzzz@gmail.com>
2022-02-09 14:08:59 +0700

    Reorgainze Code

--- lib/models/icon_menu_model.dart
+++ lib/models/icon_menu_model.dart
@@ -5,12 +5,10 @@ import 'package:travenx_loitafoundation/config/constant.dart' show iconSize;
 class ModelIconMenu {
   final Widget icon;
   final String label;
-  final Function()? onTap;
 
   const ModelIconMenu({
     required this.icon,
     required this.label,
-    this.onTap,
   });
 }
 
